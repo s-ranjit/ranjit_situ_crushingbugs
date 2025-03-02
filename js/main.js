c67a3334c6501e79a9145328d70de07c4a4c3498
Situ Ranjit <situranjit6@gmail.com>
2025-03-02 11:28:04 -0500

    fix bug no 1
    This commit contains fixing second bug where the puzzle pieces didn't change or resets while changing it's background

--- js/main.js
+++ js/main.js
@@ -18,12 +18,13 @@ dropZones = document.querySelectorAll('.drop-zone'),
 // functionality always goes in the middle -> how do we want
 // the app to behave?
 function changeBGImage() {
-	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
-	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
-	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
-	// and updating the background-image style of the puzzle board element.
-
 	// bug fix #2 should go here. it's at most 3 lines of JS code.
+	puzzlePieces.forEach(piece => {
+		// it settles the position of puzzle pieces
+          piece.style.left = '0';   
+          piece.style.top = '0';
+          document.querySelector('.puzzle-pieces').appendChild(piece);
+      });
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
